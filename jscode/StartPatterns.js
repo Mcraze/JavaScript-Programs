b8a05ae17d32dc484b9d204ba85b01237b83dab3
Mitesh Adelkar <adelkarmitesh23@gmail.com>
2025-09-18 19:18:41 +0530

    Center Star and Number Pattern

--- jscode/StartPatterns.js
+++ jscode/StartPatterns.js
@@ -62,6 +62,7 @@ function numericPyramidPattern(num) {
 }
 console.log(numericPyramidPattern(5))
 
+
 // Function to Print Numeric Pyramid Pattern
 function revNumericPyramidPattern(num) {
 
@@ -82,3 +83,101 @@ function revNumericPyramidPattern(num) {
     return singleRow;
 }
 console.log(revNumericPyramidPattern(5))
+
+
+// Function to Print Centered Pyramid Structure
+function centerPyramidStarPattern(num) {
+
+    let singleRow = "";
+
+    for (let i = 0; i < num; i++) {
+
+        for (let space = num; space > i; space--) {
+
+            singleRow += " ";
+
+        }
+
+        for (let j = 0; j <= i; j++) {
+
+            singleRow += "*";
+        }
+
+        singleRow += "\n";
+    }
+
+    return singleRow;
+}
+console.log(centerPyramidStarPattern(8));
+
+// Function to Print Reverse Cetered Pyramid Structure
+function revCenterPyramidStarPattern(num) {
+
+    let singleRow = "";
+
+    for (let i = num; i > 0; i--) {
+
+        for (let space = i; space <= num; space++) {
+            singleRow += " ";
+        }
+
+        for (let j = 0; j < i; j++) {
+            singleRow += "*";
+        }
+
+        singleRow += "\n";
+    }
+
+    return singleRow;
+}
+console.log(revCenterPyramidStarPattern(8));
+
+// Function to Print Numeric Centered Pyramid Structure
+function centerPyramidNumericPattern(num) {
+
+    let singleRow = "";
+
+    for (let i = 0; i < num; i++) {
+
+        for (let space = num; space > i; space--) {
+
+            singleRow += " ";
+
+        }
+
+        for (let j = 0; j <= i; j++) {
+
+            singleRow += "" + (j + 1);
+        }
+
+        singleRow += "\n";
+    }
+
+    return singleRow;
+}
+console.log(centerPyramidNumericPattern(8));
+
+// Function to Print Numeric Centered Pyramid Structure
+function revCenterPyramidNumericPattern(num) {
+
+    let singleRow = "";
+
+    for (let i = num; i > 0; i--) {
+
+        for (let space = i; space <= num; space++) {
+
+            singleRow += " ";
+
+        }
+
+        for (let j = 0; j < i; j++) {
+
+            singleRow += "" + (j + 1);
+        }
+
+        singleRow += "\n";
+    }
+
+    return singleRow;
+}
+console.log(revCenterPyramidNumericPattern(8));
